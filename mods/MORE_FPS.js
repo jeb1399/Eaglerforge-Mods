b1a652290f10915fcd58e11cf8980ce671b3d597
takenusername <145628605+jeb1399@users.noreply.github.com>
2024-09-05 05:52:40 -0400

    Update MORE_FPS.js

--- mods/MORE_FPS.js
+++ mods/MORE_FPS.js
@@ -1,5 +1,27 @@
 (() => {
+	// Import Settings (Cause thats all it needs)
 	ModAPI.require("settings");
+
+	// Mod Version (V.1.1)
+	const modVersion = document.createElement('div');
+	modVersion.style.position = 'absolute';
+	modVersion.style.zIndex = '1000000';
+	modVersion.style.padding = '20px';
+	modVersion.style.backgroundColor = 'rgba(127.5, 127.5, 127.5, 0.5)';
+	modVersion.style.color = "#ffffff";
+	modVersion.style.border = '1px solid #ccc';
+	modVersion.style.borderRadius = '8px';
+	modVersion.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+	modVersion.style.fontFamily = 'Arial, sans-serif';
+	modVersion.style.maxWidth = '300px';
+	modVersion.style.right = '10px';
+	modVersion.style.top = '10px';
+	const versionNum = document.createElement('p');
+	versionNum.innerText = "MORE_FPS V.1.1";
+	modVersion.appendChild(versionNum);
+	document.body.appendChild(modVersion);
+
+	// Mod
 	if (ModAPI.settings != null) {
 		if ((ModAPI.getFPS() < 29)) {
 			ModAPI.settings.renderDistanceChunks = 1;
